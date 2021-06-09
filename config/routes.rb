3255ca79018e22f775d3c9a8d9f6effe9f018159
Lenin fernando <ingeleninfernando@gmail.com>
2021-06-09 11:20:03 -0500

    crea ruta products/id

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	get"/products", to: "products#index"
 	get '/products/new', to: 'products#new', as: 'new_product'
 	post "products", to: 'products#create'
+	get '/products/:id', to: 'products#show'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
