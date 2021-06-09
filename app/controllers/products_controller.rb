8c71f733f59c219b45c1acb97c839654bc5bd668
Lenin fernando <ingeleninfernando@gmail.com>
2021-06-09 10:42:20 -0500

    implementa listar producti

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
 	def index
-		
+		@products=Product.all
+	end
+
+
+	def new
+		@products=Product.new
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'product/index'
 	get"/products", to: "products#index"
+	get'/products/new', to: 'products#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
